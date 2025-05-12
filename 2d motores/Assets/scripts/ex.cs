36de1d0e4246213bc7f0702238f55c758937a2b3
mirandak5 <miranda.lima@escolar.ifrn.edu.br>
2025-05-12 11:52:36 -0300

    exemplo
    c#

---
+++ 2d motores/Assets/scripts/ex.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class ex : MonoBehaviour
+{
+
+    private int A;
+    private int B;
+    private int C;
+    
+    
+    
+    void Start()
+    {
+        
+        A= 17;
+        B= 17; 
+        
+        
+        C = B + A;
+        Debug.Log(C);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
