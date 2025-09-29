d7e654af419db0cc40adb296811638e83e802109
mirandak5 <miranda.lima@escolar.ifrn.edu.br>
2025-09-29 08:26:10 -0300

    mola fix

--- aulas 3/Assets/scripts/mola.cs
+++ aulas 3/Assets/scripts/mola.cs
@@ -22,7 +22,7 @@ public class mola : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Mola"))
+        if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
             boost = true;
